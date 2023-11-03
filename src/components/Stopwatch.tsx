8ae6e81be247a5d163f0b4f1b453dcdc20a5c2d5
nandobfer <nandobfer@yahoo.com.br>
2023-11-03 19:09:17 -0300

    feat: working stopwatch

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -1,13 +1,56 @@
-import React from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { Box, Button } from "@mui/material"
 
 interface StopwatchProps {}
 
 export const Stopwatch: React.FC<StopwatchProps> = ({}) => {
+    const [time, setTime] = useState(60)
+    const [clocking, setClocking] = useState(false)
+
+    const resetClock = () => {
+        setTime(60)
+        setClocking(false)
+    }
+
+    const decreaseTime = useCallback(() => {
+        if (time > 0) {
+            setTime((time) => time - 1)
+        } else {
+            resetClock()
+        }
+    }, [time])
+
+    useEffect(() => {
+        if (clocking) {
+            const interval = setInterval(() => decreaseTime(), 1000)
+
+            return () => {
+                clearInterval(interval)
+            }
+        } else {
+            resetClock()
+        }
+    }, [clocking, time])
+
     return (
-        <Box sx={{ flexDirection: "column" }}>
-            {/* aqui vai ter o relogio */}
-            <Button variant="outlined">{"|>"}</Button>
+        <Box sx={{ flexDirection: "column", gap: "5vw", alignItems: "center" }}>
+            <Button
+                variant="outlined"
+                color={time > 25 ? (clocking ? "success" : "primary") : time > 10 ? "warning" : "error"}
+                onClick={() => setClocking((clocking) => !clocking)}
+                sx={{
+                    border: "1px solid",
+                    borderColor: "primary.main",
+                    borderRadius: "2vw",
+                    padding: "5vw",
+                    fontSize: "4rem",
+                    width: "80vw",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                {time}
+            </Button>
         </Box>
     )
 }
