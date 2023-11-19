9a9e4fa627c5a13836de2031fd5fa5ad6ea8c98d
nandobfer <nandobfer@yahoo.com.br>
2023-11-19 14:09:48 -0300

    feat: added bash build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ssh_profile="root@agencyboz"
+user="nando1964"
+domain="nandoburgos.dev"
+subdomain="gymagotchi.nandoburgos.dev"
+
+path="/home/${domain}/${subdomain}"
+
+yarn build
+echo 'Uploading build to server'
+scp -r build/* ${ssh_profile}:${path}
+ssh ${ssh_profile} "chown -R ${user}:${user} ${path}/*"
